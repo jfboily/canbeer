ecdc86387073119edebd42b517bcdab6881bd93c
jf boily <jf@jfboily.com>
2019-01-20 18:10:47 -0500

    Refactor rename controller

--- src/main/kotlin/com/jfboily/canbeer/controllers/ApiController.kt
+++ src/main/kotlin/com/jfboily/canbeer/controllers/ApiController.kt
@@ -4,10 +4,12 @@ import com.jfboily.canbeer.domain.repositories.BeersRepository
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
+@RequestMapping("/api/v1")
 class BeerController(val beersRepository: BeersRepository) {
 
     @GetMapping("/beers")
