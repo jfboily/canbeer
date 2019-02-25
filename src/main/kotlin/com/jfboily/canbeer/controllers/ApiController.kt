d5437d5dc786edba9366298a0b6aeef0f15dd7d0
jf boily <jf@jfboily.com>
2019-02-24 23:02:51 -0500

    Need to remove keycloak spcific securityconfig. Use oauth2 instead.

--- src/main/kotlin/com/jfboily/canbeer/controllers/ApiController.kt
+++ src/main/kotlin/com/jfboily/canbeer/controllers/ApiController.kt
@@ -2,6 +2,7 @@ package com.jfboily.canbeer.controllers
 
 import com.jfboily.canbeer.domain.repositories.BeersRepository
 import org.springframework.http.HttpStatus
+import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,6 +21,7 @@ class BeerController(val beersRepository: BeersRepository) {
             beersRepository.findById(id).orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found") }
 
     @GetMapping("/strong-beers")
+    @Secured("ROLE_USER")
     fun getStrongBeers() = beersRepository.findAll().filter { it.abv > 6 }
 
     @GetMapping("/light-beers")
